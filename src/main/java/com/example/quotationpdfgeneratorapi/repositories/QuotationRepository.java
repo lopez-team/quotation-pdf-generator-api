f19f4e2b0b8793d5acb385538a64d73b2eef8e19
Juan Pablo López <juanpablo.lopez@mercadolibre.com.co>
2023-02-16 19:37:13 -0500

    Update initial scaffolding

---
+++ src/main/java/com/example/quotationpdfgeneratorapi/repositories/QuotationRepository.java
@@ -0,0 +1,5 @@
+package com.example.quotationpdfgeneratorapi.repositories;
+
+public class QuotationRepository {
+
+}
---
+++ src/main/java/com/example/quotationpdfgeneratorapi/utils/PdfUtils.java
@@ -0,0 +1,5 @@
+package com.example.quotationpdfgeneratorapi.utils;
+
+public class PdfUtils {
+
+}
